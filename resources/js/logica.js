b9bb5de67c69419d715679733a2d468d77f8fae2
Rafael-jooj <rafaelrst15@gmail.com>
2023-08-24 18:36:41 -0300

    configurando ambiente

---
+++ resources/js/logica.js
@@ -0,0 +1,8 @@
+// const texto = document.getElementById('texto');
+// const number = document.getElementById('num').textContent;
+
+// // console.log({texto, number});
+
+// function printar(){
+//     console.log({texto, number});
+// }
